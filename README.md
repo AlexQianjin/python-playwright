0d688079f9a514b8d60d96b1125b70be9c8e06c5
Alex Qin <qianjin.qin@qq.com>
2023-07-27 14:48:51 +0800

    update the env

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # python-playwright
+
+### Setup
+```
+pip install -r .\requirements.txt
+playwright install chromium
+```
+
+### ENV
+```
+HOME_URL={}
+```
--- test_pro.py
+++ test_pro.py
@@ -1,15 +1,18 @@
+import os
+from dotenv import load_dotenv
 from playwright.sync_api import sync_playwright
 
+load_dotenv()
+HOME_URL = os.getenv("HOME_URL")
 
 def run(playwright):
     chromium = playwright.chromium
     browser = chromium.launch(headless=False, channel="chrome", args=[
                               "--start-fullscreen"])
     page = browser.new_page(no_viewport=True)
-    page.goto("https://www.jd.com")
+    page.goto(HOME_URL)
     page.set_viewport_size({"width": 3840, "height": 2160})
     # page.pause()
-    # browser.
     # browser.close()
 
 if __name__ == "__main__":
