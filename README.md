4d683c3ba9474799234e513fb3ec46eefdf5e53f
Alex Qin <qianjin.qin@qq.com>
2025-01-09 22:55:14 +0800

    implement the login

--- README.md
+++ README.md
@@ -9,4 +9,12 @@ playwright install chromium
 ### ENV
 ```
 HOME_URL={}
+USERNAME={}
+PASSWORD={}
 ```
+
+### WORKING LOG
+Date | Cost Hours | Summary
+---|---:|---
+8/1/2025 | 3 | 初始化环境
+9/1/2025 | 3 | 实现登陆
--- test_pro.py
+++ test_pro.py
@@ -4,15 +4,34 @@ from playwright.sync_api import sync_playwright
 
 load_dotenv()
 HOME_URL = os.getenv("HOME_URL")
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
 
 def run(playwright):
     chromium = playwright.chromium
     browser = chromium.launch(headless=False, channel="chrome", args=[
                               "--start-fullscreen"])
-    page = browser.new_page(no_viewport=True)
+    page = browser.new_page()
     page.goto(HOME_URL)
-    page.set_viewport_size({"width": 3840, "height": 2160})
-    # page.pause()
+    page.set_viewport_size({"width": 2560, "height": 1440})
+    # page.evaluate("() => { return window.outerWidth = screen.width; window.outerHeight = screen.height; }")
+    
+    cha = page.get_by_text("この画像に見える文字を入力してください").inner_text()
+    
+    if (cha == "この画像に見える文字を入力してください:"):
+        print(cha)
+        cha_input = page.get_by_placeholder("文字を入力してください")
+        cha_input.focus()
+        cha_input.press_sequentially("手动输入", delay=100)
+        page.evaluate("window.x = 0; setTimeout(() => { window.x = 100 }, 5000);")
+        page.wait_for_function("() => window.x > 0")
+    login = page.get_by_role("link", name="您好,请登录 账户及心愿单")
+    login.click()
+    page.get_by_label("邮箱地址").press_sequentially(USERNAME, delay=100)
+    page.get_by_label("继续").click()
+    page.get_by_label("密码").press_sequentially(PASSWORD, delay=100)
+    page.get_by_label("登录", exact=True).click()
+    page.pause()
     # browser.close()
 
 if __name__ == "__main__":
